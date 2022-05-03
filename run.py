5198f584f3525327b6d282028f7e4f143a693d23
Shaco <shanecoen@hotmail.com>
2022-05-03 10:25:41 +0000

    Convert letters to numbers for gameboards

--- run.py
+++ run.py
@@ -2,3 +2,17 @@
 YOURBOARD = [[' '] * 10 for x in range(10)]
 # Enemy ship where hits and misses are displayed
 ENEMYBOARD = [[' '] * 10 for x in range(10)]
+
+# Converting letters to numbers
+convert_letters = {
+    'A': 0, 
+    'B': 1, 
+    'C': 2, 
+    'D': 3, 
+    'E': 4,
+    'F': 5,
+    'G': 6,
+    'H': 7,
+    'I': 8,
+    'J': 9,
+    }
