09dbe19efa94d955ac74f34da98923524d96f442
Shaco <shanecoen@hotmail.com>
2022-05-03 10:19:29 +0000

    Add board variables for ship locations

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Your ship locations
+YOURBOARD = [[' '] * 10 for x in range(10)]
+# Enemy ship where hits and misses are displayed
+ENEMYBOARD = [[' '] * 10 for x in range(10)]
