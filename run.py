a26b1268768d57a798d91402b55c8de2f99fbb60
Shaco <shanecoen@hotmail.com>
2022-05-03 10:41:12 +0000

    Define function for displaying gameboard

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 # Your ship locations
 YOURBOARD = [[' '] * 10 for x in range(10)]
 # Enemy ship where hits and misses are displayed
-ENEMYBOARD = [[' '] * 10 for x in range(10)]
+ENEMYBOARD = [[' '] * 10 for i in range(10)]
 
 # Converting letters to numbers
 convert_letters = {
@@ -16,3 +16,15 @@ convert_letters = {
     'I': 8,
     'J': 9,
     }
+
+def print_gameboard(board):
+    """
+    Displays gameboard which is lettered and 
+    numbered to allow user to attack enemy ships
+    """
+    print(' A B C D E F G H I J')
+    print(' -------------------')
+    row_number = 1
+    for row in board:
+        print("%d|%s" % (row_number, "|".join(row)))
+        row_number += 1
